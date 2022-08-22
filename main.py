c7af42dedeec58b5b4d92b31f005ae5d558693a0
wassw <62353692+plerpsandplerps@users.noreply.github.com>
2022-08-22 18:15:42 -0400

    whoops lol

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = interactions.Client(token="MTAxMTM0OTI5NDQ5NTgzODMyOQ.Gvg2zG.tssIbqSl9rSMC
     description="Number between one and nine",
     scope=1011380009010724924,
 )
-async def my_first_command(ctx: interactions.CommandContext):
+async def first_command(ctx: interactions.CommandContext):
     await ctx.send("Hi there!")
 
 @bot.command(
@@ -16,7 +16,7 @@ async def my_first_command(ctx: interactions.CommandContext):
     description="Set Destination or Item target",
     scope=1011380009010724924,
 )
-async def my_first_command(ctx: interactions.CommandContext):
+async def second_command(ctx: interactions.CommandContext):
     await ctx.send("Hi there!")
 
 @bot.command(
@@ -24,7 +24,7 @@ async def my_first_command(ctx: interactions.CommandContext):
         description="Set a Player target",
         scope=1011380009010724924,
     )
-async def my_first_command(ctx: interactions.CommandContext):
+async def third_command(ctx: interactions.CommandContext):
     await ctx.send("Hi there!")
 
 @bot.command(
@@ -32,5 +32,7 @@ async def my_first_command(ctx: interactions.CommandContext):
         description="Fetch most recent printout",
         scope=1011380009010724924,
     )
-async def my_first_command(ctx: interactions.CommandContext):
+async def fourth_command(ctx: interactions.CommandContext):
     await ctx.send("Hi there!")
+
+bot.start ()
