a52117d124b519c555817be5b4c957d1af62b05b
wassw <62353692+plerpsandplerps@users.noreply.github.com>
2022-08-23 14:41:33 -0400

    traveling updates roles (no delay put in yet)

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import interactions
+from interactions import Button, ButtonStyle, SelectMenu, SelectOption, ActionRow
 import discord
 
-bot = interactions.Client(token="MTAxMTM0OTI5NDQ5NTgzODMyOQ.Gvg2zG.tssIbqSl9rSMC2vii5FOY5FLtdG5yA1U5ze0bA")
+crossroads = 1011675481403310153
+dungeon = 1011675534066974770
+farmland = 1011675659992571914
+keep = 1011675740535787530
+lichcastle = 1011675767295451228
+shop = 1011675826741325834
+tavern = 1011675868726304868
+guildid= 1011380009010724924
+bot = interactions.Client(token="MTAxMTM0OTI5NDQ5NTgzODMyOQ.Gvg2zG.tssIbqSl9rSMC2vii5FOY5FLtdG5yA1U5ze0bA", intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MESSAGE_CONTENT)
 
 @bot.event
 async def on_ready():
@@ -17,7 +26,7 @@ async def name_this_however_you_want(message: interactions.Message):
 @bot.command(
     name="lightattack",
     description="1turn. attack a player in your area for 950. gain 1rage.",
-    scope=1011380009010724924,
+    scope=guildid,
     options = [
         interactions.Option(
             name="playertarget",
@@ -33,7 +42,7 @@ async def first_command(ctx: interactions.CommandContext, playertarget: str):
 @bot.command(
     name="normalattack",
     description="2turn. attack a player in your area for 2300.gain 2rage.",
-    scope=1011380009010724924,
+    scope=guildid,
     options = [
         interactions.Option(
             name="playertarget",
@@ -49,7 +58,7 @@ async def second_command(ctx: interactions.CommandContext, playertarget: str):
 @bot.command(
     name="heavyattack",
     description="3turn. attack a player in your area for 3650.gain 3rage.",
-    scope=1011380009010724924,
+    scope=guildid,
     options = [
         interactions.Option(
             name="playertarget",
@@ -65,7 +74,7 @@ async def third_command(ctx: interactions.CommandContext, playertarget: str):
 @bot.command(
     name="interrupt",
     description="1turn. deal 4200 damage to your target if they are resting or evading",
-    scope=1011380009010724924,
+    scope=guildid,
     options = [
         interactions.Option(
             name="playertarget",
@@ -81,7 +90,7 @@ async def fourth_command(ctx: interactions.CommandContext, playertarget: str):
 @bot.command(
     name="evade",
     description="2turn. for the duration receive no damage from sources other than interrupts",
-    scope=1011380009010724924,
+    scope=guildid,
 )
 async def fifth_command(ctx: interactions.CommandContext):
     await ctx.send("You will receive no damage from sources other than interrupts.")
@@ -89,31 +98,50 @@ async def fifth_command(ctx: interactions.CommandContext):
 @bot.command(
     name="rest",
     description="2turn. heal one quarter of your missing health each turn",
-    scope=1011380009010724924,
+    scope=guildid,
 )
 async def sixth_command(ctx: interactions.CommandContext):
     await ctx.send("You will heal one quarter of your missing health each turn")
 
 @bot.command(
-    name="travel",
+    name="traveltocrossroads",
+    description="travel to the crossroads from any area",
+    scope=guildid,
+)
+async def seventh_to_command(ctx: interactions.CommandContext):
+    await ctx.send(f"You will travel to the Crossroads!")
+    await ctx.author.remove_role(dungeon, guildid)
+    await ctx.author.remove_role(farmland, guildid)
+    await ctx.author.remove_role(keep, guildid)
+    await ctx.author.remove_role(lichcastle, guildid)
+    await ctx.author.remove_role(shop, guildid)
+    await ctx.author.remove_role(tavern, guildid)
+    await ctx.author.add_role(crossroads, guildid)
+
+@bot.command(
+    name="travelfromcrossroads",
     description="set destination",
-    scope=1011380009010724924,
+    scope=guildid,
     options = [
         interactions.Option(
             name="destination",
-            description="travel to any area from the crossroads or travel from any area to the crossroads",
-            type=interactions.OptionType.STRING,
+            description="travel to any area from the crossroads",
+            type=interactions.OptionType.ROLE,
             required=True,
         ),
     ],
 )
-async def seventh_command(ctx: interactions.CommandContext, destination: str):
-    await ctx.send(f"You will travel to your '{destination}'!")
+async def seventh_from_command(ctx: interactions.CommandContext, destination: str):
+    if crossroads not in ctx.author.roles:
+        return await ctx.send("You aren't in the crossroads! You must use / traveltocrossroads first!")
+    await ctx.send(f"You will travel to the {destination.mention}!")
+    await ctx.author.remove_role(crossroads, guildid)
+    await ctx.author.add_role(destination, guildid)
 
 @bot.command(
     name="exchange",
     description="choose a player in the area. give that player an unused item from your inventory",
-    scope=1011380009010724924,
+    scope=guildid,
     options = [
         interactions.Option(
             name="unusedinventoryitem",
@@ -135,7 +163,7 @@ async def eigth_first_command(ctx: interactions.CommandContext, playertarget: st
 @bot.command(
     name="loot",
     description="roll 1d4. on 4 or higher gain two random items. low roll loses 0.25 of their current health",
-    scope=1011380009010724924,
+    scope=guildid,
 )
 async def eigth_second_command(ctx: interactions.CommandContext):
     await ctx.send("You will attempt to loot the dungeon!")
@@ -143,7 +171,7 @@ async def eigth_second_command(ctx: interactions.CommandContext):
 @bot.command(
     name="farm",
     description="roll 1d4. gain that many seed coins.",
-    scope=1011380009010724924,
+    scope=guildid,
 )
 async def eigth_third_command(ctx: interactions.CommandContext):
     await ctx.send("You will gain 1d4 seed coins!")
@@ -151,7 +179,7 @@ async def eigth_third_command(ctx: interactions.CommandContext):
 @bot.command(
     name="aid",
     description="roll 1d4. high roll heals their chosen player for 0.25 of their missing health",
-    scope=1011380009010724924,
+    scope=guildid,
     options = [
         interactions.Option(
             name="playertarget",
@@ -167,7 +195,7 @@ async def eigth_fourth_command(ctx: interactions.CommandContext, playertarget: s
 @bot.command(
     name="battlelich",
     description="roll 1d4. if you are the highest roller and rolled 5 gain the lich item",
-    scope=1011380009010724924,
+    scope=guildid,
 )
 async def eigth_fifth_command(ctx: interactions.CommandContext):
     await ctx.send("You will attempt to battle the lich!")
@@ -175,7 +203,7 @@ async def eigth_fifth_command(ctx: interactions.CommandContext):
 @bot.command(
     name="trade",
     description="exchange seed coins for an item",
-    scope=1011380009010724924,
+    scope=guildid,
     options = [
         interactions.Option(
             name="shopitem",
@@ -186,12 +214,12 @@ async def eigth_fifth_command(ctx: interactions.CommandContext):
     ],
 )
 async def eigth_sixth_command(ctx: interactions.CommandContext, shopitem:str):
-    await ctx.send("You will purchase '{shopitem}' !")
+    await ctx.send(f"You will purchase '{shopitem}' !")
 
 @bot.command(
     name="drinkingchallenge",
     description="roll 1d4. high roll adds 420 damage to a random attack. low roller loses 0.25 of their current hp",
-    scope=1011380009010724924,
+    scope=guildid,
 )
 async def eigth_seventh_command(ctx: interactions.CommandContext):
     await ctx.send("You will try to best the rest in a drinking challenge!")
@@ -199,7 +227,7 @@ async def eigth_seventh_command(ctx: interactions.CommandContext):
 @bot.command(
     name="useitem",
     description="choose an unused item in your inventory and use it",
-    scope=1011380009010724924,
+    scope=guildid,
     options = [
         interactions.Option(
             name="unusedinventoryitem",
@@ -210,12 +238,12 @@ async def eigth_seventh_command(ctx: interactions.CommandContext):
     ],
 )
 async def ninth_command(ctx: interactions.CommandContext, unusedinventoryitem: str):
-    await ctx.send("You will use '{unusedinventoryitem}'!")
+    await ctx.send(f"You will use '{unusedinventoryitem}'!")
 
 @bot.command(
         name="printout",
         description="fetch most recent printout",
-        scope=1011380009010724924,
+        scope=guildid,
     )
 async def tenth_command(ctx: interactions.CommandContext):
     await ctx.send("DM me if you want access to the test env")
