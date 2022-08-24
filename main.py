f232744e85042ad782a9dc96b691cda1be810362
wassw <62353692+plerpsandplerps@users.noreply.github.com>
2022-08-24 12:26:01 -0400

    starting json

--- main.py
+++ main.py
@@ -5,37 +5,8 @@ import asyncio
 from datetime import datetime
 import time
 import math
+import json
 
-from setuptools import setup
-import re
-
-with open("interactions/ext/checks/__init__.py") as f:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
-    ).group(1)
-
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-setup(
-    name="interactions-checks",
-    version=version,
-    description="Add in check decorators for discord-py-interactions",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Catalyst4222/interactions-checks",
-    author="Catalyst4",
-    author_email="catalyst4222@gmail.com",
-    license="MIT",
-    packages=["interactions.ext.checks"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=["discord-py-interactions>=4.0.0"],
-)
 
 crossroads = 1011675481403310153
 dungeon = 1011675534066974770
@@ -45,7 +16,7 @@ lichcastle = 1011675767295451228
 shop = 1011675826741325834
 tavern = 1011675868726304868
 guildid= 1011380009010724924
-poisonchannel= 1011380009568587878
+poisonchannel= 1011701650798424185
 bot = interactions.Client(token="MTAxMTM0OTI5NDQ5NTgzODMyOQ.Gvg2zG.tssIbqSl9rSMC2vii5FOY5FLtdG5yA1U5ze0bA", intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MESSAGE_CONTENT)
 
 @bot.event
@@ -73,10 +44,23 @@ async def on_ready():
             await asyncio.sleep(countdown)
 
 @bot.event(name="on_message_create")
-async def name_this_however_you_want(message: interactions.Message):
+async def listen(message: interactions.Message):
     print(
         f"We've received a message from {message.author.username}. The message is: {message.content}."
     )
+
+@bot.command(
+    name="join",
+    description="join the game!",
+    scope=guildid,
+)
+async def join_command(USER):
+    with open ("players.json","r") as f:
+        players = json.load(f)
+    if str(USER.id) in players:
+        return False
+    else:
+        players[str(USER.id)]
 
 @bot.command(
     name="lightattack",
