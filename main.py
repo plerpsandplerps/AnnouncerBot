974d5ffb8c963dd56d7d8d7b5592616bc260110a
wassw <62353692+plerpsandplerps@users.noreply.github.com>
2022-08-25 10:39:26 -0400

    adding comments for collaborators

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ guildid= 1011380009010724924
 poisonchannel= 1011380009568587878
 bot = interactions.Client(token="MTAxMTM0OTI5NDQ5NTgzODMyOQ.Gvg2zG.tssIbqSl9rSMC2vii5FOY5FLtdG5yA1U5ze0bA", intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MESSAGE_CONTENT)
 
+# poison to world.json to keep it consistent between script reboots
+# poison needs damage to actually be added
+
 @bot.event
 async def on_ready():
     print(f"We're online! We've logged in as {bot.me.name}.")
@@ -49,11 +52,13 @@ async def listen(message: interactions.Message):
         f"We've received a message from {message.author.username}. The message is: {message.content}."
     )
 
+#pulls player.json into dict
 async def getplayerdata():
     with open("players.json","r") as f:
         players = json.load(f)
     return players
 
+#this pulls the bounty data into dict
 async def getbountydata():
     with open("bounties.json","r") as g:
         startingbounties = json.load(g)
@@ -66,7 +71,11 @@ async def getbountydata():
 )
 async def join_command(ctx: interactions.CommandContext):
     players = await getplayerdata()
-    #bounties = await getbountydata()
+    # need to add:
+    # check for bounties
+    # bounties = await getbountydata()
+    # depends on adding poison to world.json first:
+        # check for poisondate (if join b4 poison, 10SC+bounty and 10000hp, otherwise min(SC)-1 and MIN(HP)-1)
     if str(ctx.author.id) in players:
         await ctx.send(f"Failed to Join! {ctx.author} already exists as a player! ")
         return False
@@ -75,8 +84,8 @@ async def join_command(ctx: interactions.CommandContext):
         current_time = int(time.time())
         countdown=int(300)
         #if str(ctx.author.id) in bounties:
-        #    await ctx.send(f"{ctx.author} has claimed existing bounties!")
         #    bounty_pull = startingbounties[str(ctx.author.id)]["bounty"]
+        #    await ctx.send(f"{ctx.author} has claimed prior bounties for {bounty_pull}!")
         players[str(ctx.author.id)] = {}
         players[str(ctx.author.id)]["HP"] = 10000
         players[str(ctx.author.id)]["Location"] = "Crossroads"
@@ -88,6 +97,7 @@ async def join_command(ctx: interactions.CommandContext):
         players[str(ctx.author.id)]["Delay"] = True
         players[str(ctx.author.id)]["Evade"] = False
         players[str(ctx.author.id)]["Rest"] = False
+        players[str(ctx.author.id)]["Lastaction"] = "Start"
         with open("players.json","w") as f:
             json.dump(players,f, indent=4)
         print(f"Created {ctx.author.id} player in players.json")
@@ -102,13 +112,20 @@ async def join_command(ctx: interactions.CommandContext):
         Delay_pull = players[str(ctx.author.id)]["Delay"]
         Evade_pull = players[str(ctx.author.id)]["Evade"]
         Rest_pull = players[str(ctx.author.id)]["Rest"]
+        Lastaction_pull = players[str(ctx.author.id)]["Lastaction"]
         await ctx.send(f"{ctx.author}'s HP: {hp_pull} \nLocation: {location_pull} \nSC: {SC_pull} \nRage: {Rage_pull} \nInventory: \n    Ready: {ReadyInventory_pull} \n    Used:{UsedInventory_pull} \nDelay: <t:{DelayDate_pull}> ({Delay_pull})")
         await asyncio.sleep(countdown)
-        await ctx.send(f"@{ctx.author} your countdown is over!")
+        await ctx.send(f"<@{ctx.author.id}> your countdown is over!")
+
+# add damage for all attacks
+# add buff check for all attacks (usedinventory)
+# add target hp print
+# limit targets to those in area
+# add message for target to see
 
 @bot.command(
     name="lightattack",
-    description="1turn. attack a player in your area for 950. gain 1rage.",
+    description="24h. attack a player in your area for 950. gain 1rage.",
     scope=guildid,
     options = [
         interactions.Option(
@@ -121,11 +138,11 @@ async def join_command(ctx: interactions.CommandContext):
 )
 async def first_command(ctx: interactions.CommandContext, playertarget: str):
     players = await getplayerdata()
-    await ctx.send(f"You will use light attack on '{playertarget}'!", ephemeral=False)
+    await ctx.send(f"You use light attack on '{playertarget}'!", ephemeral=False)
 
 @bot.command(
     name="normalattack",
-    description="2turn. attack a player in your area for 2300.gain 2rage.",
+    description="48h. attack a player in your area for 2300. gain 2rage.",
     scope=guildid,
     options = [
         interactions.Option(
@@ -138,11 +155,11 @@ async def first_command(ctx: interactions.CommandContext, playertarget: str):
 )
 async def second_command(ctx: interactions.CommandContext, playertarget: str):
     players = await getplayerdata()
-    await ctx.send(f"You will use normal attack on '{playertarget}'!", ephemeral=False)
+    await ctx.send(f"You use normal attack on '{playertarget}'!", ephemeral=False)
 
 @bot.command(
     name="heavyattack",
-    description="3turn. attack a player in your area for 3650.gain 3rage.",
+    description="72h. attack a player in your area for 3650. gain 3rage.",
     scope=guildid,
     options = [
         interactions.Option(
@@ -155,11 +172,11 @@ async def second_command(ctx: interactions.CommandContext, playertarget: str):
 )
 async def third_command(ctx: interactions.CommandContext, playertarget: str):
     players = await getplayerdata()
-    await ctx.send(f"You will use heavy attack on '{playertarget}'!", ephemeral=False)
+    await ctx.send(f"You use heavy attack on '{playertarget}'!", ephemeral=False)
 
 @bot.command(
     name="interrupt",
-    description="1turn. deal 4200 damage to your target if they are resting or evading",
+    description="24h. deal 4200 damage to your target if they are resting or evading",
     scope=guildid,
     options = [
         interactions.Option(
@@ -172,20 +189,20 @@ async def third_command(ctx: interactions.CommandContext, playertarget: str):
 )
 async def fourth_command(ctx: interactions.CommandContext, playertarget: str):
     players = await getplayerdata()
-    await ctx.send(f"You will use interrupt on '{playertarget}'!", ephemeral=False)
+    await ctx.send(f"You use interrupt on '{playertarget}'!", ephemeral=False)
 
 @bot.command(
     name="evade",
-    description="2turn. for the duration receive no damage from sources other than interrupts",
+    description="48h. for the duration receive no damage from sources other than interrupts",
     scope=guildid,
 )
 async def fifth_command(ctx: interactions.CommandContext):
     players = await getplayerdata()
-    await ctx.send("You will receive no damage from sources other than interrupts.", ephemeral=False)
+    await ctx.send("You receive no damage from sources other than interrupts.", ephemeral=False)
 
 @bot.command(
     name="rest",
-    description="2turn. heal one quarter of your missing health each turn",
+    description="48h. heal one quarter of your missing health each turn",
     scope=guildid,
 )
 async def sixth_command(ctx: interactions.CommandContext):
@@ -194,7 +211,7 @@ async def sixth_command(ctx: interactions.CommandContext):
 
 @bot.command(
     name="traveltocrossroads",
-    description="travel to the crossroads from any area",
+    description="24h. travel to the crossroads from any area",
     scope=guildid,
 )
 async def seventh_to_command(ctx: interactions.CommandContext):
@@ -210,7 +227,7 @@ async def seventh_to_command(ctx: interactions.CommandContext):
 
 @bot.command(
     name="travelfromcrossroads",
-    description="set destination",
+    description="24h. travel anywhere from the crossroads",
     scope=guildid,
     options = [
         interactions.Option(
@@ -231,7 +248,7 @@ async def seventh_from_command(ctx: interactions.CommandContext, destination: st
 
 @bot.command(
     name="exchange",
-    description="choose a player in the area. give that player an unused item from your inventory",
+    description="choose a player in your area. give that player an unused item from your inventory",
     scope=guildid,
     options = [
         interactions.Option(
@@ -254,11 +271,12 @@ async def eigth_first_command(ctx: interactions.CommandContext, playertarget: st
 
 @bot.command(
     name="loot",
-    description="roll 1d4. on 4 or higher gain two random items. low roll loses 0.25 of their current health",
+    description="roll 1d100. high score: get two random items. low score: lose 0.25 of your current health",
     scope=guildid,
 )
 async def eigth_second_command(ctx: interactions.CommandContext):
     players = await getplayerdata()
+    roll = random.randint(1,100)
     await ctx.send("You attempt to loot the dungeon!", ephemeral=False)
 
 @bot.command(
@@ -268,11 +286,12 @@ async def eigth_second_command(ctx: interactions.CommandContext):
 )
 async def eigth_third_command(ctx: interactions.CommandContext):
     players = await getplayerdata()
+    roll = random.randint(1,4)
     await ctx.send("You gain 1d4 seed coins!", ephemeral=False)
 
 @bot.command(
     name="aid",
-    description="roll 1d4. high roll heals their chosen player for 0.25 of their missing health",
+    description="roll 1d100. high score: heal chosen player for 0.25 of their missing health",
     scope=guildid,
     options = [
         interactions.Option(
@@ -284,14 +303,16 @@ async def eigth_third_command(ctx: interactions.CommandContext):
     ],
 )
 async def eigth_fourth_command(ctx: interactions.CommandContext, playertarget: str):
+    roll = random.randint(1,100)
     await ctx.send(f"You heal '{playertarget}'!", ephemeral=False)
 
 @bot.command(
     name="battlelich",
-    description="roll 1d4. if you are the highest roller and rolled 5 gain the lich item",
+    description="roll 1d100. high score > 90: gain magic orb",
     scope=guildid,
 )
 async def eigth_fifth_command(ctx: interactions.CommandContext):
+    roll = random.randint(1,100)
     players = await getplayerdata()
     await ctx.send("You defeat the lich!", ephemeral=False)
 
@@ -314,7 +335,7 @@ async def eigth_sixth_command(ctx: interactions.CommandContext, shopitem:str):
 
 @bot.command(
     name="drinkingchallenge",
-    description="roll 1d4. high roll adds 420 damage to a random attack. low roller loses 0.25 of their current hp",
+    description="roll 1d100. high score: permanently add 420 damage to a random attack. low score: lose 0.25 current hp",
     scope=guildid,
 )
 async def eigth_seventh_command(ctx: interactions.CommandContext):
@@ -345,6 +366,17 @@ async def ninth_command(ctx: interactions.CommandContext, unusedinventoryitem: s
     )
 async def tenth_command(ctx: interactions.CommandContext):
     players = await getplayerdata()
+        hp_pull = players[str(ctx.author.id)]["HP"]
+        location_pull = players[str(ctx.author.id)]["Location"]
+        SC_pull = players[str(ctx.author.id)]["SC"]
+        Rage_pull = players[str(ctx.author.id)]["Rage"]
+        ReadyInventory_pull = players[str(ctx.author.id)]["ReadyInventory"]
+        UsedInventory_pull = players[str(ctx.author.id)]["UsedInventory"]
+        DelayDate_pull = players[str(ctx.author.id)]["DelayDate"]
+        Delay_pull = players[str(ctx.author.id)]["Delay"]
+        Evade_pull = players[str(ctx.author.id)]["Evade"]
+        Rest_pull = players[str(ctx.author.id)]["Rest"]
+        Lastaction_pull = players[str(ctx.author.id)]["Lastaction"]
     await ctx.send(f"{ctx.author}'s HP: {hp_pull} \nLocation: {location_pull} \nSC: {SC_pull} \nRage: {Rage_pull} \nInventory: \n    Ready: {ReadyInventory_pull} \n    Used:{UsedInventory_pull} \nDelay: <t:{DelayDate_pull}> ({Delay_pull})", ephemeral=True)
 
 bot.start ()
