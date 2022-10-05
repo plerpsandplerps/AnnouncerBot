c2b149c10fa9351d5228ed52b824493b84a5386a
wassw <liamwassmer@gmail.com>
2022-10-05 13:54:44 -0400

    loot added to functiondict

--- main.py
+++ main.py
@@ -1800,6 +1800,7 @@ functiondict = {'lightattack' : dolightattack,
                 'beerbando':dobeerbando,
                 'aimtrain':doaimtrain,
                 'crookedabacus':docrookedabacus,
+                'loot':doloot,
                 'trade':dotrade}
 
 
